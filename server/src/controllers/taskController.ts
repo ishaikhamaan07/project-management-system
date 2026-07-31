4bf283c6ad12a3886ffdf468b681e6afc0c947e8
ishaikhamaan07 <ishaikhamaan07@gmail.com>
2026-07-31 15:57:06 +0530

    Add task creation and retrieval APIs

---
+++ server/src/controllers/taskController.ts
@@ -0,0 +1,86 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const getTasks = async (
+    req: Request,
+    res: Response
+): Promise<void> => {
+    const { projectId } = req.query;
+
+    try {
+        const tasks = await prisma.task.findMany({
+            where: {
+                projectId: Number(projectId),
+            },
+            include: {
+                author: true,
+                assignee: true,
+                comments: true,
+                attachments: true,
+            },
+        });
+
+        res.json(tasks);
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json({
+                message: `Error retrieving tasks: ${error.message}`,
+            });
+        } else {
+            res.status(500).json({
+                message: "Unknown error retrieving tasks",
+            });
+        }
+    }
+};
+
+export const createTask = async (
+    req: Request,
+    res: Response
+): Promise<void> => {
+    const {
+        title,
+        description,
+        status,
+        priority,
+        tags,
+        startDate,
+        dueDate,
+        points,
+        projectId,
+        authorUserId,
+        assignedUserId,
+    } = req.body;
+
+    try {
+        const newTask = await prisma.task.create({
+            data: {
+                title,
+                description,
+                status,
+                priority,
+                tags,
+                startDate,
+                dueDate,
+                points,
+                projectId,
+                authorUserId,
+                assignedUserId,
+            },
+        });
+
+        res.status(201).json(newTask);
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json({
+                message: `Error creating a task: ${error.message}`,
+            });
+        } else {
+            res.status(500).json({
+                message: "Unknown error creating task",
+            });
+        }
+    }
+};
