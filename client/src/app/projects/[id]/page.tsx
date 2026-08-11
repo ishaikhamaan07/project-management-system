1b6dfb423ab10c174d26de18a9bf9a529e42bc20
ishaikhamaan07 <ishaikhamaan07@gmail.com>
2026-08-11 14:03:35 +0530

    Implement initial Kanban board layout

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react'
 import ProjectHeader from "@/app/projects/ProjectHeader";
+import Board from '../BoardView';
 
 type Props = {
     params: {id: string}
@@ -15,9 +16,9 @@ const Project = ({ params }: Props) => {
   return <div>
     {/* MODAL NEW TASK */}
     <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
-    {/* {activeTab === "Board" &&(
-        <Board />
-    )} */}
+    {activeTab === "Board" &&(
+        <Board id ={id} setIsModalNewTaskOpen = {setIsModalNewTaskOpen}/>
+    )} 
   </div>
   
 }
