d3d32f1d8116d7d454b771bf38cba41d73169b60
ishaikhamaan07 <ishaikhamaan07@gmail.com>
2026-08-23 11:30:22 +0530

    Implement create task modal and API integration

--- client/src/app/projects/TableView/index.tsx
+++ client/src/app/projects/TableView/index.tsx
@@ -78,7 +78,17 @@ const TableView = ({ id, setIsModalNewTaskOpen } : Props) => {
 
   return <div className='h-[540px] w-full px-4 pb-8 xl:px-6'>
     <div className='pt-5 '>
-      <Header name="Table" isSmallText/>
+      <Header name="Table" 
+        buttonComponent={
+                <button
+                className='flex items-center rounded bg-blue-primary px-3 py-2 text-white hover:bg-blue-6000'
+                onClick={() => setIsModalNewTaskOpen(true)}
+                >
+                    Add Task
+                </button>
+            }
+        isSmallText
+      />
     </div>
     <DataGrid rows={tasks || []} columns={columns}
       className={dataGridClassNames}
