235187b115d436fb5af6521fa9fb36f5178619cf
ishaikhamaan07 <ishaikhamaan07@gmail.com>
2026-09-04 10:58:52 +0530

    Add authenticated user profile controls

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -2,7 +2,8 @@
 
     import { useAppDispatch, useAppSelector } from "@/app/redux";
     import { setIsSidebarCollapsed } from "@/state";
-    import { useGetProjectsQuery } from "@/state/api";
+    import { useGetAuthUserQuery, useGetProjectsQuery } from "@/state/api";
+    import { signOut } from "aws-amplify/auth";
     import { AlertCircle, AlertOctagon, AlertTriangle, Briefcase, ChevronDown, ChevronUp, Home, Layers3, LockIcon, LucideIcon, Search, Settings, ShieldAlert, User, Users, X } from "lucide-react";
     import Image from "next/image";
     import Link from "next/link";
@@ -18,6 +19,18 @@
         const isSidebarCollapsed = useAppSelector(
             (state) => state.global.isSidebarCollapsed,
         );
+
+        const { data: currentUser } = useGetAuthUserQuery({});
+        const handleSignOut = async () => {
+            try {
+                await signOut();
+            }   catch (error) {
+                console.error("Error signing out: ", error)
+            }
+        };
+
+        if (!currentUser) return null;
+        const currentUserDetails = currentUser?.userDetails;
 
         const sidebarClassNames =  `fixed flex flex-col h-[100%]  justify-between shadow-xl
             transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white
@@ -126,6 +139,32 @@
                             />
                         </>
                     )}
+            </div>
+            <div className="z-10 mt-32 flex w-full flex-col items-center gap-4 bg-white px-8 py-4 dark:bg-black md:hidden">
+            <div className='flex w-full items-center'>
+                <div className='align-center flex h-9 w-9 justify-center'>
+                    {!!currentUserDetails?.profilePictureUrl ? (
+                        <Image
+                            src={`https://pm-s3-images-amaan.s3.ap-south-1.amazonaws.com/${currentUserDetails?.profilePictureUrl}`}
+                            alt={currentUserDetails?.username || "User Profile Picture"}
+                            width={100}
+                            height={50}
+                            className="h-full rounded-full object-cover"
+                        />
+                        ) : (
+                          <User className="h-6 w-6 cursor-pointer self-center rounded-full dark:text-white" />
+                        )}
+                        </div>
+                        <span className='mx-3 text-gray-800 dark:text-white'>
+                            {currentUserDetails?.username}
+                        </span>
+                        <button
+                            className='self-start rounded bg-blue-400 px-4 py-2 text-xs font-bold text-white hover:bg-blue-500 md:block'
+                            onClick={handleSignOut}
+                        >
+                            Sign out
+                        </button>
+                </div>
             </div>
         </div>
         )
