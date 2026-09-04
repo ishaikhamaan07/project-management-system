1634e64e72f5ed3ebe0429ced1d548a0134a59e0
ishaikhamaan07 <ishaikhamaan07@gmail.com>
2026-09-04 10:48:42 +0530

    Add authenticated user data retrieval

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { fetchAuthSession, getCurrentUser } from "aws-amplify/auth";
 
 export interface Project {
     id: number;
@@ -78,6 +79,24 @@ export const api = createApi({
     reducerPath: "api",
     tagTypes:["Projects", "Tasks", "Users", "Teams"],
     endpoints: (build) => ({
+        getAuthUser: build.query({
+            queryFn: async (_, _queryApi, _extraoptions, fetchWithBQ) => {
+                try {
+                  const user = await getCurrentUser();
+                  const session = await fetchAuthSession();
+                  if (!session) throw new Error("No session found");
+                  const { userSub } = session;
+                  const { accessToken } = session.tokens ?? {};
+
+                  const userDetailsResponse = await fetchWithBQ(`users/${userSub}`);
+                  const userDetails = userDetailsResponse.data as User;
+
+                  return { data: { user, userSub, userDetails}}
+                } catch (error: any) {
+                  return { error: error.message || "Could not fetch user data"}
+                }
+            }
+        }),
         getProjects: build.query<Project[], void>({
             query: () => "projects",
             providesTags: ["Projects"]
@@ -146,5 +165,5 @@ export const {
     useGetUsersQuery,
     useGetTeamsQuery,
     useGetTasksByUserQuery,
-
+    useGetAuthUserQuery,
 } = api;
