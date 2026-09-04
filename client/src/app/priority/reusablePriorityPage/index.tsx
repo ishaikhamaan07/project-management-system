f81e1943d885fe2ce8f9ca9c5bb8747fbfd24a01
ishaikhamaan07 <ishaikhamaan07@gmail.com>
2026-09-04 11:04:00 +0530

    Use authenticated user for priority tasks

--- client/src/app/priority/reusablePriorityPage/index.tsx
+++ client/src/app/priority/reusablePriorityPage/index.tsx
@@ -5,7 +5,7 @@ import Header from '@/components/Header'
 import ModalNewTask from '@/components/ModalNewTask'
 import TaskCard from '@/components/TaskCard'
 import { dataGridClassNames, dataGridSxStyles } from '@/lib/utils'
-import { Priority, Task, useGetTasksByUserQuery } from '@/state/api'
+import { Priority, Task, useGetAuthUserQuery, useGetTasksByUserQuery } from '@/state/api'
 import { DataGrid, GridColDef } from '@mui/x-data-grid'
 import React, { useState } from 'react'
 
@@ -73,8 +73,13 @@ const ReusablePriorityPage = ({ priority }: Props) => {
     const [view, setView ] = useState("list");
     const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
     
-    const userId = 1;
-    const { data: tasks, isLoading, isError: isTasksError} = useGetTasksByUserQuery(userId || 0, {
+    const { data: currentUser } = useGetAuthUserQuery({});
+    const userId =  currentUser?.userDetails?.userId ?? null;
+    const { 
+        data: tasks, 
+        isLoading, 
+        isError: isTasksError
+    } = useGetTasksByUserQuery(userId || 0, {
         skip: userId === null
     })
 
